c3c043aab91b8c7e1c4cbb110fd49776ce4e498f
Mahdi <mbd7793@gmail.com>
2025-09-09 21:45:52 -0500

    import List which was missing

--- plotters/statistical/heatmap.py
+++ plotters/statistical/heatmap.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 from core.base_plotter import BasePlotter, PlotConfig
 
 
